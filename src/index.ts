c3c1368f9d222650c625c27906245e5d3063e7a8
Geovani S. Junior <geovanij3@gmail.com>
2022-04-09 18:21:50 -0300

    Add GET /boleto/:digits

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { BankSlipInterface, BankSlip, Dealership } from "../lib";
 
+import express from "express";
+
 function main(str: string) {
   const foo: BankSlipInterface =
     str.length === 47 ? new BankSlip(str) : new Dealership(str);
@@ -12,6 +14,22 @@ function main(str: string) {
     expirationDate: foo.expirationDate(),
   };
 }
+
+const app = express();
+
+app.get("/boleto/:digits", (req, res) => {
+  const { digits } = req.params;
+
+  try {
+    const body = main(digits);
+    console.log(body);
+    res.status(200).json(body);
+  } catch (error) {
+    res.status(400).end();
+  }
+});
+
+app.listen(8080);
 
 console.log(main("21290001192110001210904475617405975870000002000"));
 console.log(main("00190500954014481606906809350314337370000000100"));
